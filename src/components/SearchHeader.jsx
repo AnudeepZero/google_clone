43dd4d24b8da0af03e9f8461336c048836dd4a64
AnudeepZero <anudeepsambaraju@gmail.com>
2023-05-08 14:15:10 +0530

    Layout header

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import SearchBox from './SearchBox'
+import {AiOutlineSetting} from 'react-icons/ai'
+import {BsGrid3X3Gap} from 'react-icons/bs'
+import SearchHeaderOption from './SearchHeaderOption'
+
 
 export default function SearchHeader() {
   return (
-    <div>SearchHeader</div>
+    <header className='sticky top-0 bg-white' >
+    <div className='flex w-full p-6 items-center justify-between' >    
+    
+    <div  >
+      <Link href="/">
+      <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png" width={120} height={40} alt='Google'/>
+      </Link>
+    </div>
+    
+    <div className='flex-1' >
+      <SearchBox />
+    </div>
+
+    <div className='hidden md:inline-flex space-x-2' >
+      <AiOutlineSetting className='header-icon' />
+      <BsGrid3X3Gap className='header-icon' />
+    </div>
+
+    <button className='bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2' >Sign in</button>
+    </div>
+    <SearchHeaderOption />
+    </header>
   )
 }
